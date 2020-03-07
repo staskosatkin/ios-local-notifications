9459cd375f00f4d27f921d60466a506820f804b4
Stanislav Kosatkin <stas.kosatkin@gmail.com>
2020-03-07 12:35:08 +0200

    Initial Commit

---
+++ NotificationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotificationTest
+//
+//  Created by Stanislav  Kosatkin on 07.03.2020.
+//  Copyright © 2020 Stanislav  Kosatkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
